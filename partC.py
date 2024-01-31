62eae67891f85c66cc78fdb871e66af598fdd68c
B00152372 Aleena Gillani <B00152372@mytudublin.ie>
2024-01-31 12:26:39 +0000

    PartC.py file added

---
+++ partC.py
@@ -0,0 +1,11 @@
+#Aleena Gillani
+#31/01/2024
+#A Program to Print Steps 1 to 9 to the Screen
+def printsteps(a, b):
+    for i in range(a, b+1):
+        print(f"steps {i}")
+        
+    
+
+printsteps(1,9)
+
